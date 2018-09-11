d2cf47e23f6da8b5ce6b0b933d6a532698f61f48
Christian Gonzalez <1581488+christiango@users.noreply.github.com>
2018-09-10 20:04:46 -0700

    Sample api call and log it to console

--- src/index.jsx
+++ src/index.jsx
@@ -5,4 +5,9 @@ import { FlickrApiKey } from '../apiKey';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-console.log(FlickrApiKey);
+fetch(
+  `https://api.flickr.com/services/rest/?method=flickr.photos.search&tags=cats&api_key=${FlickrApiKey}&format=json&nojsoncallback=1`
+).then(async response => {
+  const json = await response.json();
+  console.log(json);
+});
