a3b8e23e883bdd26c0da3e15fcf8001527d3abd7
Christian Gonzalez <1581488+christiango@users.noreply.github.com>
2018-09-10 21:45:08 -0700

    Basic layout

--- src/PreviewPane.jsx
+++ src/PreviewPane.jsx
@@ -1,17 +1,18 @@
 import * as React from 'react';
 
 const PreviewCard = props => (
-  <div>
-    <img src={props.photoUrl} />
+  <div style={{ padding: 10 }}>
+    <img src={props.photoUrl} style={{ height: '100%' }} />
   </div>
 );
 
 export class PreviewPane extends React.PureComponent {
   render() {
     return (
-      <div>
+      <div style={{ display: 'flex', height: 200, justifyContent: 'center' }}>
         {this.props.items.map((item, index) => (
           <PreviewCard
+            key={index}
             photoUrl={item.photoUrl}
             isSelected={index === this.props.selectedIndex}
           />
