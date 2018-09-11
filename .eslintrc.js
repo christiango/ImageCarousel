7ad6eda7f96077fc4c42f679317172987d5703f8
Christian Gonzalez <1581488+christiango@users.noreply.github.com>
2018-09-10 22:47:11 -0700

    Tweak linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,12 @@
 module.exports = {
-  extends: 'airbnb',
+  extends: 'eslint:recommended',
   rules: {
     // Turn off the rules I don't agree with
-    'import/prefer-default-export': false,
-    'react/prefer-stateless-function': false
+    'comma-dangle': 0,
+    'arrow-parens': 0,
+    'no-unused-vars': 0 // false positive for react import
   },
+  parser: 'babel-eslint',
   env: {
     browser: true
   }
